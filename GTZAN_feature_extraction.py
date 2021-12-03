60676b710ffe5a890abdd1b1a76f1794132afec0
PilvioSol <polsilvio@gmail.com>
2021-12-03 16:32:46 +0100

    script to save spectrograms added, to be modified

--- GTZAN_feature_extraction.py
+++ GTZAN_feature_extraction.py
@@ -1,14 +1,23 @@
-
+import os
 import numpy as np
 import pathlib
 import matplotlib.pyplot as plt
 import librosa.display
 import librosa
 
+path = "/nas/home/spol/Thesis/GTZAN/images"
+
+try:
+    os.mkdir(path)
+except OSError:
+    print("Creation of the directory %s failed" % path)
+else:
+    print("Successfully created the directory %s " % path)
 
 _SAMPLING_RATE = 16000
 print('ollare')
 data_dir = pathlib.Path('GTZAN/genres_original/blues')
+
 
 def extract_features(file_name):
     try:
@@ -23,16 +32,29 @@ def extract_features(file_name):
 
 
 files_in_basepath = data_dir.iterdir()
-'''for item in files_in_basepath:
+i = 0
+for item in files_in_basepath:
+
     if item.is_file():
         print(item.name)
-    features = extract_features(item)'''
+        features = extract_features(item)
+        name = 'spectrogram' + str(i)
+        fig, ax = plt.subplots()
+        img = librosa.display.specshow(librosa.amplitude_to_db(features, ref=np.max),
+                                       sr=_SAMPLING_RATE, x_axis='time', y_axis='cqt_note', ax=ax)
+        ax.set_title('Constant-Q power spectrum')
+        fig.colorbar(img, ax=ax, format="%+2.0f dB")
+        fig.savefig('/nas/home/spol/Thesis/GTZAN/images/' + name)
+    i = i + 1
 
-sample = pathlib.Path('GTZAN/genres_original/blues/blues.00000.wav')
-features = extract_features(sample)
-fig, ax = plt.subplots()
-img = librosa.display.specshow(librosa.amplitude_to_db(features, ref=np.max),
-                               sr=_SAMPLING_RATE, x_axis='time', y_axis='cqt_note', ax=ax)
-ax.set_title('Constant-Q power spectrum')
-fig.colorbar(img, ax=ax, format="%+2.0f dB")
-plt.show()
+
+
+
+
+
+
+
+
+# sample = pathlib.Path('GTZAN/genres_original/blues/blues.00000.wav')
+
+# plt.show()
