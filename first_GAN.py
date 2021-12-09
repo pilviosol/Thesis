d4251f6c91b68a9b244a213f20694452302f9fab
PilvioSol <polsilvio@gmail.com>
2021-12-09 18:48:52 +0100

    added CycleGAN.py and pix2pix.py

--- first_GAN.py
+++ first_GAN.py
@@ -10,11 +10,18 @@ import time
 
 from IPython import display
 
+'''
 (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
 
 train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
-train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
+train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1] '''
 
+train_images = "/nas/home/spol/Thesis/GTZAN/images/"
+for image in train_images:
+    image = image.reshape(image.shape[0], 28, 28, 1).astype('float32')
+    image = (image - 127.5) / 127.5
+
+'''
 BUFFER_SIZE = 60000
 BATCH_SIZE = 256
 
@@ -188,6 +195,6 @@ with imageio.get_writer(anim_file, mode='I') as writer:
     image = imageio.imread(filename)
     writer.append_data(image)
   image = imageio.imread(filename)
-  writer.append_data(image)
+  writer.append_data(image) '''
 
 
