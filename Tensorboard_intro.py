8ac03eb69d0b47886f4ffbd3da18ef0c9fab5fa0
PilvioSol <polsilvio@gmail.com>
2021-12-30 20:12:06 +0100

    Added Tensorboard_intro.py, first tries with Tensorboard

---
+++ Tensorboard_intro.py
@@ -0,0 +1,86 @@
+# ssh -L 6006:localhost:6006 spol@dirac
+# !rm -rf logs
+# %tensorboard --logdir logs/train_data
+'''import tensorflow as tf
+import datetime
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+
+def create_model():
+    return tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+
+
+model = create_model()
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+model.fit(x=x_train,
+          y=y_train,
+          epochs=5,
+          validation_data=(x_test, y_test),
+          callbacks=[tensorboard_callback])'''
+
+
+#-----------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
+
+from datetime import datetime
+import io
+import itertools
+from packaging import version
+
+import tensorflow as tf
+from tensorflow import keras
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn.metrics
+
+print("TensorFlow version: ", tf.__version__)
+assert version.parse(tf.__version__).release[0] >= 2, \
+    "This notebook requires TensorFlow 2.0 or above."
+
+# Download the data. The data is already divided into train and test.
+# The labels are integers representing classes.
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = \
+    fashion_mnist.load_data()
+
+# Names of the integer classes, i.e., 0 -> T-short/top, 1 -> Trouser, etc.
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+    'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print("Shape: ", train_images[0].shape)
+print("Label: ", train_labels[0], "->", class_names[train_labels[0]])
+
+# Reshape the image for the Summary API.
+img = np.reshape(train_images[0], (-1, 28, 28, 1))
+
+# Clear out any prior log data.
+
+
+# Sets up a timestamped log directory.
+logdir = "logs/train_data/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+# Creates a file writer for the log directory.
+file_writer = tf.summary.create_file_writer(logdir)
+
+# Using the file writer, log the reshaped image.
+with file_writer.as_default():
+  tf.summary.image("Training data", img, step=0)
