2860ed9384c3e00df6284783c0f3a18c6888dcb9
PilvioSol <polsilvio@gmail.com>
2022-01-13 20:02:36 +0100

    added files

---
+++ src/__init__.py
@@ -0,0 +1,16 @@
+"""Here we can see how to import functions and submodules of our projects."""
+
+# more complex: import submodules if a package is installed
+from importlib.util import find_spec
+
+# this line imports two elements defined in "arguments"
+from .arguments import read_args, write_args
+# this line imports all the functions of "utils"
+from .utils import *
+
+if find_spec("torch"):
+    from . import torch_utils
+
+if find_spec("tensorflow"):
+    from . import tensorflow_utils
+
---
+++ src/tensorflow_utils.py
@@ -0,0 +1,44 @@
+"""
+Tensorflow functions
+
+Blank Project
+
+Image and Sound Processing Lab - Politecnico di Milano
+
+Paolo Bestagini
+"""
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+# Tell "from tensorflow import *" what to import
+__all__ = [
+    "init_gpus",
+    "model_keras",
+]
+
+
+def init_gpus():
+    """Set memory growth"""
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+
+
+def model_keras(input_shape):
+    """
+    Simple CNN model
+    :param input_shape: input data shape
+    :return: CNN model
+    """
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(10))
+    model.add(layers.Softmax())
+
+    return model
