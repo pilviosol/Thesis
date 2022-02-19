62bb13dca41d8cb540fa9d9c9d9b9269db1ab9fe
PilvioSol <polsilvio@gmail.com>
2022-02-19 16:53:20 +0100

    commented

--- CREPE_test.py
+++ CREPE_test.py
@@ -1,9 +1,7 @@
 import crepe
-from scipy.io import wavfile
 import librosa
 import matplotlib.pyplot as plt
 
-# sr, audio = wavfile.read('AUDIO_TEST/scala.wav')
 audio, sr = librosa.load('AUDIO_TEST/scala.wav', res_type='kaiser_fast', mono=True)
 time, frequency, confidence, activation = crepe.predict(audio, sr, viterbi=True)
 
