921cc86ee2822b741692c061f20c68a574b39de7
PilvioSol <polsilvio@gmail.com>
2022-03-03 19:05:19 +0100

    added ddsp files somehow

--- DDSP_test.py
+++ DDSP_test.py
@@ -1,19 +1,62 @@
-'''
-#@title Install and import dependencies
+import sys
+sys.path.insert(1, '/nas/home/spol/Thesis/ddsp-main/ddsp')
 
-%tensorflow_version 2.x
-!pip install -qU ddsp
-
-# Ignore a bunch of deprecation warnings
 import warnings
-warnings.filterwarnings("ignore")
-
-import ddsp
-import ddsp.training
-from ddsp.colab.colab_utils import play, specplot, DEFAULT_SAMPLE_RATE
+import copy
+import os
+import time
+import crepe
+# import gin
+import librosa
 import matplotlib.pyplot as plt
 import numpy as np
-import tensorflow as tf
+import pickle
 
+# import tensorflow_datasets as tfds
+warnings.filterwarnings("ignore")
+
+
+
+# Helper Functions
+DEFAULT_SAMPLE_RATE = 16000
 sample_rate = DEFAULT_SAMPLE_RATE  # 16000
+
+print('Done!')
+
+# --------------------------------------------------------------------
+audio, sr = librosa.load('scala_sustained.wav', sr=DEFAULT_SAMPLE_RATE)
+if len(audio.shape) == 1:
+    audio = audio[np.newaxis, :]
+print('\nExtracting audio features...')
+
+
+# Setup the session.
+reset_crepe()
+
+'''
+# Compute features.
+start_time = time.time()
+audio_features = ddsp.training.metrics.compute_audio_features(audio)
+audio_features['loudness_db'] = audio_features['loudness_db'].astype(np.float32)
+audio_features_mod = None
+print('Audio features took %.1f seconds' % (time.time() - start_time))
+
+
+TRIM = -15
+# Plot Features.
+fig, ax = plt.subplots(nrows=3, 
+                       ncols=1, 
+                       sharex=True,
+                       figsize=(6, 8))
+ax[0].plot(audio_features['loudness_db'][:TRIM])
+ax[0].set_ylabel('loudness_db')
+
+ax[1].plot(librosa.hz_to_midi(audio_features['f0_hz'][:TRIM]))
+ax[1].set_ylabel('f0 [midi]')
+
+ax[2].plot(audio_features['f0_confidence'][:TRIM])
+ax[2].set_ylabel('f0 confidence')
+_ = ax[2].set_xlabel('Time step [frame]')
+
+
 '''
