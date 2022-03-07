c2f6d031cdfdb3f551c33579e78d310e0895b201
PilvioSol <polsilvio@gmail.com>
2022-03-07 12:19:27 +0100

    added prova code for resampling

--- prova_STFT.py
+++ prova_STFT.py
@@ -1,9 +1,22 @@
 import librosa
 import librosa.display
+import scipy.io.wavfile
 import numpy as np
 import matplotlib.pyplot as plt
 
 y, sr = librosa.load('/nas/home/spol/Thesis/scala_sustained.wav')
+print('y.shape', y.shape)
+print('original sr', sr)
+
+new_sr = 8000
+y_8k = librosa.resample(y, orig_sr=sr, target_sr=new_sr)
+scipy.io.wavfile.write('/nas/home/spol/Thesis/resampled_scala_sustained.wav', new_sr, y_8k)
+
+y_hat, sr_new = librosa.load('/nas/home/spol/Thesis/resampled_scala_sustained.wav', sr=None)
+print('y_hat.shape', y_hat.shape)
+print('new sr', sr_new)
+
+'''
 S1 = np.abs(librosa.stft(y))
 
 
@@ -29,3 +42,4 @@ ax.set_title('Power spectrogram')
 fig.colorbar(img, ax=ax, format="%+2.0f dB")
 plt.show()
 print('debiug')
+'''
