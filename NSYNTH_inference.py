3d196d48b4632e4218e78f039378bd6ee708bfec
PilvioSol <polsilvio@gmail.com>
2022-03-18 17:42:54 +0100

    inference pipeline finished but LQ results

--- NSYNTH_inference.py
+++ NSYNTH_inference.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import librosa.display
 import pathlib
 import numpy as np
+import pickle
 
 # ---------------------------------------------------------------------------------------------------------------------
 # PATH, VARIABLES
@@ -12,7 +13,7 @@ test_path_file = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/testing_flute
 test_path_feature = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/testing_flute_features/"
 normalised_test_path_feature = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/normalised_testing_flute_features/"
 min_max_flute_inference_path_file = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/inference_flute_min_max.npy"
-
+denormalised_spectrogram_path = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/denormalised_spectrogram/"
 
 # ---------------------------------------------------------------------------------------------------------------------
 # CALCULATE THE SPECTROGRAM SPECTROGRAM
@@ -25,9 +26,9 @@ feature_calculation(test_path_file, test_path_feature)
 files_dir = pathlib.Path(test_path_feature)
 files_in_basepath = files_dir.iterdir()
 for file in files_in_basepath:
-
     fig, ax = plt.subplots()
-    img = librosa.display.specshow(file, y_axis='log', x_axis='time', ax=ax)
+    spectrogram = np.load(file)
+    img = librosa.display.specshow(spectrogram, y_axis='linear', x_axis='time', ax=ax)
     ax.set_title('Flute Sample Power spectrogram')
     fig.colorbar(img, ax=ax, format="%+2.0f dB")
     plt.show()
@@ -54,18 +55,45 @@ def generate(spectrograms):
 files_dir = pathlib.Path(normalised_test_path_feature)
 files_in_basepath = files_dir.iterdir()
 for file in files_in_basepath:
-    generated_spectrogram, latent_representation = generate(file)
+    spectrogram = np.load(file)
+    spectrogram = np.expand_dims(spectrogram, axis=0)
+    spectrogram = np.expand_dims(spectrogram, axis=-1)
+    print('spectrogram expanded dims: ', spectrogram.shape)
+
+    generated_spectrogram, latent_representation = generate(spectrogram)
 
 
 # ---------------------------------------------------------------------------------------------------------------------
 # GET THE OUTPUT AND DE-NORMALISE IT
 # ---------------------------------------------------------------------------------------------------------------------
 
+min_max_values = np.load(min_max_flute_inference_path_file)
+generated_spectrogram = np.squeeze(generated_spectrogram)
+
+
+def denormalise(norm_array, original_min, original_max):
+    array = (norm_array - norm_array.min()) / (norm_array.max() - norm_array.min())
+    array = array * (original_max - original_min) + original_min
+    return array
+
+
+denormalised_spectrogram = denormalise(generated_spectrogram, min_max_values[0][0], min_max_values[0][1])
+np.save(denormalised_spectrogram_path + 'denormalised_spectrogram.npy', denormalised_spectrogram)
+
 
 # ---------------------------------------------------------------------------------------------------------------------
 # PLOT THE RECONSTRUCTED SPECTROGRAM
 # ---------------------------------------------------------------------------------------------------------------------
-
+files_dir = pathlib.Path(denormalised_spectrogram_path)
+files_in_basepath = files_dir.iterdir()
+for file in files_in_basepath:
+    print(file)
+    fig, ax = plt.subplots()
+    spectrogram = np.load(file)
+    img = librosa.display.specshow(spectrogram, y_axis='linear', x_axis='time', ax=ax)
+    ax.set_title('Denormalised spectrogram')
+    fig.colorbar(img, ax=ax, format="%+2.0f dB")
+    plt.show()
 
 # ---------------------------------------------------------------------------------------------------------------------
 # SYNTHESIZE THE NEW AUDIO
@@ -74,3 +102,4 @@ for file in files_in_basepath:
 
 
 
+print('debug')
