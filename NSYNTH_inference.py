cbbc1de3fd727d00983b62a5a868804a0aa52ed4
PilvioSol <polsilvio@gmail.com>
2022-03-23 18:07:32 +0100

    refined NSYNTH_inference.py.

--- NSYNTH_inference.py
+++ NSYNTH_inference.py
@@ -4,48 +4,73 @@ import matplotlib.pyplot as plt
 import librosa.display
 import pathlib
 import numpy as np
+import scipy.io.wavfile
 import pickle
 
 # ---------------------------------------------------------------------------------------------------------------------
 # PATH, VARIABLES
 # ---------------------------------------------------------------------------------------------------------------------
-test_path_file = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/testing_flute/"
-test_path_feature = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/testing_flute_features/"
-normalised_test_path_feature = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/normalised_testing_flute_features/"
-min_max_flute_inference_path_file = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/inference_flute_min_max.npy"
-denormalised_spectrogram_path = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/denormalised_spectrogram/"
+test_path_file = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/INFERENCE_flute/"
+test_path_feature = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/INFERENCE_flute_features/"
+normalised_test_path_feature = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/INFERENCE_normalised_flute_features/"
+generated_test_path_feature = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/INFERENCE_generated_flute_features/"
+min_max_flute_inference_path_file = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/INFERENCE_flute_min_max.npy"
+denormalised_spectrogram_path = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/INFERENCE_denormalised_spectrogram/"
+generated_vocal_path = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/INFERENCE_generated_vocal/"
+
+
+print('PATH, VARIABLES..........ok')
+
 
 # ---------------------------------------------------------------------------------------------------------------------
-# CALCULATE THE SPECTROGRAM
+# CALCULATE SPECTROGRAMS
 # ---------------------------------------------------------------------------------------------------------------------
 feature_calculation(test_path_file, test_path_feature)
 
+
+print('CALCULATE SPECTROGRAMS..........ok')
+
+
 # ---------------------------------------------------------------------------------------------------------------------
-# PLOT THE SPECTROGRAM
+# PLOT THE SPECTROGRAMS
 # ---------------------------------------------------------------------------------------------------------------------
 files_dir = pathlib.Path(test_path_feature)
 files_in_basepath = files_dir.iterdir()
 for file in files_in_basepath:
-    fig, ax = plt.subplots()
+    name = file.name
     spectrogram = np.load(file)
-    img = librosa.display.specshow(spectrogram, y_axis='linear', x_axis='time', ax=ax)
-    ax.set_title('Flute Sample Power spectrogram')
-    fig.colorbar(img, ax=ax, format="%+2.0f dB")
+    fig = plt.figure()
+    img = plt.imshow(spectrogram, cmap=plt.cm.viridis, origin='lower', extent=[0, 256, 0, 512],
+                     aspect='auto')
+    plt.title(name)
+    plt.colorbar()
     plt.show()
 
+
+print('PLOT THE SPECTROGRAMS..........ok')
+
+
 # ---------------------------------------------------------------------------------------------------------------------
-# NORMALIZE THE SPECTROGRAM AND SAVE MIN MAX
+# NORMALIZE THE SPECTROGRAMS AND SAVE MIN MAX
 # ---------------------------------------------------------------------------------------------------------------------
 min_max_flute_inference = normalise_set_and_save_min_max(test_path_feature, normalised_test_path_feature)
 np.save(min_max_flute_inference_path_file, min_max_flute_inference)
+
+
+print('NORMALIZE THE SPECTROGRAMS AND SAVE MIN MAX..........ok')
+
+
 # ---------------------------------------------------------------------------------------------------------------------
 # IMPORT THE MODEL
 # ---------------------------------------------------------------------------------------------------------------------
-vae = VAE.load("/nas/home/spol/Thesis/saved_model/VV_model")
+vae = VAE.load("/nas/home/spol/Thesis/saved_model/VV_model_x_train_y_train_val02_new_formula")
+
+
+print('IMPORT THE MODEL..........ok')
 
 
 # ---------------------------------------------------------------------------------------------------------------------
-# FEED IT TO THE MODEL
+# FEED THEM TO THE MODEL
 # ---------------------------------------------------------------------------------------------------------------------
 def generate(spectrograms):
     generated_spectrograms, latent_representations = vae.reconstruct(spectrograms)
@@ -55,45 +80,74 @@ def generate(spectrograms):
 files_dir = pathlib.Path(normalised_test_path_feature)
 files_in_basepath = files_dir.iterdir()
 for file in files_in_basepath:
+    name = file.name
     spectrogram = np.load(file)
     spectrogram = np.expand_dims(spectrogram, axis=0)
     spectrogram = np.expand_dims(spectrogram, axis=-1)
     print('spectrogram expanded dims: ', spectrogram.shape)
 
     generated_spectrogram, latent_representation = generate(spectrogram)
+    np.save(generated_test_path_feature + 'GENERATED_' + name, generated_spectrogram)
+
+
+print('FEED THEM TO THE MODEL..........ok')
 
 
 # ---------------------------------------------------------------------------------------------------------------------
-# GET THE OUTPUT AND DE-NORMALISE IT
+# GET THE OUTPUT AND DE-NORMALISE THEM
 # ---------------------------------------------------------------------------------------------------------------------
 
 min_max_values = np.load(min_max_flute_inference_path_file)
-generated_spectrogram = np.squeeze(generated_spectrogram)
+
+files_dir = pathlib.Path(generated_test_path_feature)
+files_in_basepath = files_dir.iterdir()
+for idx, file in enumerate(files_in_basepath):
+    name = file.name
+    gen_spectrogram = np.load(file)
+    gen_spectrogram = np.squeeze(gen_spectrogram)
+    denormalised_spectrogram = denormalise(gen_spectrogram, min_max_values[idx][0], min_max_values[idx][1])
+    np.save(denormalised_spectrogram_path + name, denormalised_spectrogram)
 
 
-denormalised_spectrogram = denormalise(generated_spectrogram, min_max_values[0][0], min_max_values[0][1])
-np.save(denormalised_spectrogram_path + 'denormalised_spectrogram.npy', denormalised_spectrogram)
+print('GET THE OUTPUT AND DE-NORMALISE THEM..........ok')
 
 
 # ---------------------------------------------------------------------------------------------------------------------
-# PLOT THE RECONSTRUCTED SPECTROGRAM
+# PLOT THE RECONSTRUCTED SPECTROGRAMS
 # ---------------------------------------------------------------------------------------------------------------------
 files_dir = pathlib.Path(denormalised_spectrogram_path)
 files_in_basepath = files_dir.iterdir()
 for file in files_in_basepath:
-    print(file)
-    fig, ax = plt.subplots()
-    spectrogram = np.load(file)
-    img = librosa.display.specshow(spectrogram, y_axis='linear', x_axis='time', ax=ax)
-    ax.set_title('Denormalised spectrogram')
-    fig.colorbar(img, ax=ax, format="%+2.0f dB")
+    name = file.name
+    print(name)
+    denorm_spectrogram = np.load(file)
+    fig = plt.figure()
+    img = plt.imshow(denorm_spectrogram, cmap=plt.cm.viridis, origin='lower', extent=[0, 256, 0, 512],
+                     aspect='auto')
+    plt.title(name)
+    plt.colorbar()
     plt.show()
+
+
+print('PLOT THE RECONSTRUCTED SPECTROGRAMS..........ok')
+
 
 # ---------------------------------------------------------------------------------------------------------------------
 # SYNTHESIZE THE NEW AUDIO
 # ---------------------------------------------------------------------------------------------------------------------
-
-
+files_dir = pathlib.Path(denormalised_spectrogram_path)
+files_in_basepath = files_dir.iterdir()
+for file in files_in_basepath:
+    name = file.name
+    name = name[0:-4]
+    print(name)
+    denorm_spectrogram = np.load(file)
+    '''
+    print('denorm_shape: ', denorm_spectrogram.shape)
+    padded_denorm_spectrogram = np.pad(denorm_spectrogram, ((0, 0), (0, 1024-256)), 'constant') 
+    print('padded_denorm_shape: ', padded_denorm_spectrogram.shape) '''
+    reconstructed = librosa.griffinlim(denorm_spectrogram)
+    scipy.io.wavfile.write(generated_vocal_path + 'REVERSED_' + name + '.wav', 16000, reconstructed)
 
 
 print('debug')
--- plot_spectrogram_from_wav.py
+++ plot_spectrogram_from_wav.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 import librosa.display
 import math
 
-test_path = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/testing_flute/"
-test_path_feature = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/testing_flute_features/"
-test_path_file = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/testing_flute/021_flute_acoustic_027-021-025.wav"
+test_path = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/INFERENCE_flute/"
+test_path_feature = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/INFERENCE_flute_features/"
+test_path_file = "/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/INFERENCE_flute/flute_acoustic_002-079-100.wav"
 
 
 audio, sample_rate = librosa.load(test_path_file, res_type='kaiser_fast', mono=True, sr=None)
@@ -20,8 +20,13 @@ plt.title('Power spectrogram FROM FILE')
 plt.colorbar()
 plt.show()
 # -------------------------------------------------------------------------------------------------
+spectr = np.load("/nas/home/spol/Thesis/NSYNTH/NSYNTH_TRAIN_SUBSET/INFERENCE_flute_features/flute_acoustic_002-079-100_STFTMAG_NEW.npy")
 
-
+fig = plt.figure()
+img = plt.imshow(10*np.log10(spectr + 1e-1), cmap=plt.cm.viridis, origin='lower', extent=[0,501,0,513], aspect='auto')
+plt.title('Power spectrogram FROM FILE')
+plt.colorbar()
+plt.show()
 
 
 
