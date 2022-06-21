18f1f03ca00d58b6a406561877d4fc7128022b15
PilvioSol <polsilvio@gmail.com>
2022-06-21 18:49:07 +0200

    now working confusion matrix

--- Classifier_inference.py
+++ Classifier_inference.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pathlib
 from functions import load_fsdd
@@ -27,14 +28,16 @@ classes = [0, 1, 2, 3]
 y_val = []
 for i in range(352):
     if i < 88:
-        y_val.append(classes[0])
+        y_val.append(classes[2])
     elif 88 <= i < 176:
         y_val.append(classes[1])
     elif 176 <= i < 264:
-        y_val.append(classes[2])
-    else:
         y_val.append(classes[3])
+    else:
+        y_val.append(classes[0])
 y_val = np.asarray(y_val)
+
+print(y_val)
 
 labels=["string", "keyboard", "guitar", "organ"]
 # ---------------------------------------------------------------
@@ -45,14 +48,18 @@ probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
 generated_spectrograms = load_fsdd(path)
 
 predictions = probability_model.predict(generated_spectrograms)
-
+arg_pred_all = []
 
 for i in range(352):
-    print(i, 'predictions: ', np.around(predictions[i], 3), 'class: ', np.argmax(predictions[i]))
+    # print(i, 'predictions: ', np.around(predictions[i], 3), 'class: ', np.argmax(predictions[i]))
+    arg_pred_all.append(np.argmax(predictions[i]))
+
+print(arg_pred_all)
 
 y_true = y_val
-y_pred = predictions
+y_pred = np.asarray(arg_pred_all)
 cm = confusion_matrix(y_true, y_pred, labels=classes)
 
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=classes)
 disp.plot()
+plt.show()
