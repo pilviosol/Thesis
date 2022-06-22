6817cc962eac4f5b292b8e01b92121caba21d71b
PilvioSol <polsilvio@gmail.com>
2022-06-22 12:38:05 +0200

    finished Classifier_interpolation_all.py now having the arrays with all the predictions

--- Classifier_interpolation_all.py
+++ Classifier_interpolation_all.py
@@ -18,6 +18,10 @@ KG_path = main_path + "KG/"
 GO_path = main_path + "GO/"
 SO_path = main_path + "SO/"
 
+KS_save_path = main_path + "KS_predictions"
+KG_save_path = main_path + "KG_predictions"
+GO_save_path = main_path + "GO_predictions"
+SO_save_path = main_path + "SO_predictions"
 
 # ---------------------------------------------------------------
 #  LOAD CLASSIFIER
@@ -29,12 +33,48 @@ probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
 
 
 # ---------------------------------------------------------------
-#  KS PREDICTIONS
+#  KS PREDICTIONS (ROW0)
 # ---------------------------------------------------------------
 
-interpolations = load_fsdd(KS_path)
+ks_interpolations = load_fsdd(KS_path)
+ks_interpolations = np.squeeze(ks_interpolations)
+ks_interpolations = np.expand_dims(ks_interpolations, axis=-1)
 
-predictions = probability_model.predict(interpolations)
+ks_predictions = probability_model.predict(ks_interpolations)
+np.save(KS_save_path, ks_predictions)
+
+# ---------------------------------------------------------------
+#  KG PREDICTIONS (COLUMN4)
+# ---------------------------------------------------------------
+
+kg_interpolations = load_fsdd(KG_path)
+kg_interpolations = np.squeeze(kg_interpolations)
+kg_interpolations = np.expand_dims(kg_interpolations, axis=-1)
+
+kg_predictions = probability_model.predict(kg_interpolations)
+np.save(KG_save_path, kg_predictions)
+
+# ---------------------------------------------------------------
+#  GO PREDICTIONS (ROW4)
+# ---------------------------------------------------------------
+
+go_interpolations = load_fsdd(GO_path)
+go_interpolations = np.squeeze(go_interpolations)
+go_interpolations = np.expand_dims(go_interpolations, axis=-1)
+
+go_predictions = probability_model.predict(go_interpolations)
+np.save(GO_save_path, go_predictions)
+
+# ---------------------------------------------------------------
+#  SO PREDICTIONS (COLUMN0)
+# ---------------------------------------------------------------
+
+so_interpolations = load_fsdd(SO_path)
+so_interpolations = np.squeeze(so_interpolations)
+so_interpolations = np.expand_dims(so_interpolations, axis=-1)
+
+so_predictions = probability_model.predict(so_interpolations)
+np.save(SO_save_path, so_predictions)
 
 print('debug')
 
