51fea205d24e4a389dfa95215a5f0c5ec4c316c6
PilvioSol <polsilvio@gmail.com>
2022-06-22 16:17:56 +0200

    added plot all together in perceptual

--- box_plots_perceptual.py
+++ box_plots_perceptual.py
@@ -10,9 +10,10 @@ point_2 = np.concatenate((a[:, 0], a[:, 3], a[:, 6], a[:, 9]))
 point_3 = np.concatenate((a[:, 1], a[:, 4], a[:, 7], a[:, 10]))
 point_4 = np.concatenate((a[:, 2], a[:, 5], a[:, 8], a[:, 11]))
 
+all = [point_2, point_3, point_4]
 
 # ---------------------------------------------------------------
-#  BOX PLOTS -POINT 2
+#  BOX PLOTS - POINT 2
 # ---------------------------------------------------------------
 
 
@@ -23,7 +24,7 @@ plt.show()
 
 
 # ---------------------------------------------------------------
-#  BOX PLOTS -POINT 3
+#  BOX PLOTS - POINT 3
 # ---------------------------------------------------------------
 
 
@@ -34,13 +35,23 @@ plt.show()
 
 
 # ---------------------------------------------------------------
-#  BOX PLOTS -POINT 4
+#  BOX PLOTS - POINT 4
 # ---------------------------------------------------------------
 
 
 fig1, ax1 = plt.subplots()
 ax1.set_title('POINT 4')
 ax1.boxplot(point_4, showmeans=True)
+plt.show()
+
+
+# ---------------------------------------------------------------
+#  BOX PLOTS - ALL TOGETHER
+# ---------------------------------------------------------------
+
+fig1, ax1 = plt.subplots()
+ax1.set_title('ALL')
+ax1.boxplot(all, showmeans=True)
 plt.show()
 
 print('debug')
